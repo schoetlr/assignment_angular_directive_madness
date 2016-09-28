2b0340fd11b166ed797b5842afd546422ff5c6bc
schoetlr <srlrs20020@gmail.com>
2016-09-27 21:48:18 -0400

    finished attribute directive

--- app.js
+++ app.js
@@ -9,3 +9,46 @@ DirectiveMadness.directive('mainNav', function(){
     scope: {}
   }
 });
+
+
+DirectiveMadness.directive("mainHeader", function(){
+
+  return {
+    templateUrl: "mainheader.html",
+    restrict: "E",
+    transclude: true,
+    scope: {}
+  }
+});
+
+
+DirectiveMadness.directive("copyright", function(){
+
+  return {
+    template: "<p style='text-align: center'><ng-transclude></ng-transclude></p>",
+    restrict: "E",
+    transclude: true,
+    link: function(scope, element, attrs){
+      element.text("Copyrighted Company 2016");
+      //this is taking away the text-align center css
+      //and the following isn't fixing it either
+      element.css("text-align", "center");
+    }
+  }
+});
+
+
+DirectiveMadness.directive("colorize", function(){
+
+  return {
+    restrict: "A",
+    scope: {
+      color: "@",
+      background: "@"
+    },
+    link: function(scope, element, attributes){
+      element.css("color", attributes.color);
+      element.css("background-color", attributes.background);
+    }
+  }
+})
